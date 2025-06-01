77eae47a65e9e6146e894d136b6f4eea52902b8b
Pavel Yakovlev <thrnrhhl@yandex.ru>
2025-06-01 21:43:36 +0300

    UNT-25 [interview-task-take my code-backend] The package.json fix
    Additional fixes

--- index.js
+++ index.js
@@ -91,4 +91,4 @@ app.post("/sort-dnd", (req, res) => {
   items = update;
   res.json({ update });
 });
-app.listen(3001, () => console.log("\u0421\u0435\u0440\u0432\u0435\u0440 \u0437\u0430\u043F\u0443\u0449\u0435\u043D \u043D\u0430 \u043F\u043E\u0440\u0442\u0443 3001"));
+app.listen(3e3, () => console.log("\u0421\u0435\u0440\u0432\u0435\u0440 \u0437\u0430\u043F\u0443\u0449\u0435\u043D \u043D\u0430 \u043F\u043E\u0440\u0442\u0443 3001"));
